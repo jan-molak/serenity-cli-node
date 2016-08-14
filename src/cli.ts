ba67c4b74e76a93b5b22b857ef4e83fa828896bd
jan-molak <jan.molak@gmail.com>
2016-08-14 14:36:45 +0100

    fix(api): The bootrap script is exprted as a function, so that requiring the module doesn't have a s

--- src/cli.ts
+++ src/cli.ts
@@ -3,18 +3,21 @@ import winston = require('winston');
 
 import { logger } from './logger';
 
-logger.add(winston.transports.Console, { colorize: true });
+export = function bootstrap() {
 
-yargs
-    .version(() => require('../package.json').version)
-    .demand(1)
-    .boolean('verbose')
-    .usage('Usage: $0 <command> [options]')
-    .example('$0 update [options]', 'updates the Serenity jar to the latest version')
-    .example('$0 remove [options]', 'removes the cache directory and downloaded jars')
-    .example('$0 run [options]',    'generates the HTML report')
-    .example('$0 <command> --help', 'shows the available parameters')
-    .epilog('copyright (C) Jan Molak <jan.molak@smartcodeltd.co.uk>')
-    .commandDir('commands')
-    .alias('h', 'help').help()
-    .argv;
+    logger.add(winston.transports.Console, { colorize: true });
+
+    yargs
+        .version(() => require('../package.json').version)
+        .demand(1)
+        .boolean('verbose')
+        .usage('Usage: $0 <command> [options]')
+        .example('$0 update [options]', 'updates the Serenity jar to the latest version')
+        .example('$0 remove [options]', 'removes the cache directory and downloaded jars')
+        .example('$0 run [options]',    'generates the HTML report')
+        .example('$0 <command> --help', 'shows the available parameters')
+        .epilog('copyright (C) Jan Molak <jan.molak@smartcodeltd.co.uk>')
+        .commandDir('commands')
+        .alias('h', 'help').help()
+        .argv;
+};
